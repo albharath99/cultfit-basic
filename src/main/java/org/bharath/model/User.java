9e62d503bdef41d6f496e471efc50a2fb5365c7c
Bharath A L <albharath99@gmail.com>
2024-10-16 22:25:24 +0530

    Create classes

---
+++ src/main/java/org/bharath/model/User.java
@@ -0,0 +1,12 @@
+package org.bharath.model;
+
+import java.util.List;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private List<BookedSession> bookedSessionList;
+
+
+}
